aef2cdda34f1a563c5a4c67182511dc47fc9a048
Max <max@studio107.ru>
2016-03-17 19:31:36 +0300

    Remove unused code

--- src/Mindy/Console/ConsoleCommand.php
+++ src/Mindy/Console/ConsoleCommand.php
@@ -75,9 +75,17 @@ abstract class ConsoleCommand
      * @since 1.1.5
      */
     public $defaultAction = 'index';
-
+    /**
+     * @var string
+     */
     private $_name;
+    /**
+     * @var ConsoleCommandRunner
+     */
     private $_runner;
+    /**
+     * @var \Mindy\Base\Module
+     */
     private $_module;
 
     /**
@@ -104,7 +112,6 @@ abstract class ConsoleCommand
      */
     public function beforeAction($action, $params)
     {
-        return true;
     }
 
     /**
@@ -117,33 +124,6 @@ abstract class ConsoleCommand
      */
     public function afterAction($action, $params, $exitCode)
     {
-        return $exitCode;
-    }
-
-    /**
-     * Returns a list of behaviors that this command should behave as.
-     * The return value should be an array of behavior configurations indexed by
-     * behavior names. Each behavior configuration can be either a string specifying
-     * the behavior class or an array of the following structure:
-     * <pre>
-     * 'behaviorName'=>array(
-     *     'class'=>'path.to.BehaviorClass',
-     *     'property1'=>'value1',
-     *     'property2'=>'value2',
-     * )
-     * </pre>
-     *
-     * Note, the behavior classes must implement {@link IBehavior} or extend from
-     * {@link CBehavior}. Behaviors declared in this method will be attached
-     * to the controller when it is instantiated.
-     *
-     * For more details about behaviors, see {@link CComponent}.
-     * @return array the behavior configurations (behavior name=>behavior configuration)
-     * @since 1.1.11
-     */
-    public function behaviors()
-    {
-        return [];
     }
 
     /**
@@ -217,8 +197,8 @@ abstract class ConsoleCommand
      */
     public function getModule()
     {
-        if($this->_module === null) {
-            $reflect = new \ReflectionClass(get_class($this));
+        if ($this->_module === null) {
+            $reflect = new ReflectionClass(get_class($this));
             $namespace = $reflect->getNamespaceName();
             $segments = explode('\\', $namespace);
             $this->_module = Mindy::app()->getModule($segments[1]);
@@ -351,142 +331,6 @@ abstract class ConsoleCommand
     {
         echo "Error: $message\n\n" . $this->getHelp() . "\n";
         exit(1);
-    }
-
-    /**
-     * Copies a list of files from one place to another.
-     * @param array $fileList the list of files to be copied (name=>spec).
-     * The array keys are names displayed during the copy process, and array values are specifications
-     * for files to be copied. Each array value must be an array of the following structure:
-     * <ul>
-     * <li>source: required, the full path of the file/directory to be copied from</li>
-     * <li>target: required, the full path of the file/directory to be copied to</li>
-     * <li>callback: optional, the callback to be invoked when copying a file. The callback function
-     *   should be declared as follows:
-     *   <pre>
-     *   function foo($source,$params)
-     *   </pre>
-     *   where $source parameter is the source file path, and the content returned
-     *   by the function will be saved into the target file.</li>
-     * <li>params: optional, the parameters to be passed to the callback</li>
-     * </ul>
-     * @see buildFileList
-     */
-    public function copyFiles($fileList)
-    {
-        $overwriteAll = false;
-        foreach ($fileList as $name => $file) {
-            $source = strtr($file['source'], '/\\', DIRECTORY_SEPARATOR);
-            $target = strtr($file['target'], '/\\', DIRECTORY_SEPARATOR);
-            $callback = isset($file['callback']) ? $file['callback'] : null;
-            $params = isset($file['params']) ? $file['params'] : null;
-
-            if (is_dir($source)) {
-                $this->ensureDirectory($target);
-                continue;
-            }
-
-            if ($callback !== null) {
-                $content = call_user_func($callback, $source, $params);
-            } else {
-                $content = file_get_contents($source);
-            }
-            if (is_file($target)) {
-                if ($content === file_get_contents($target)) {
-                    echo "  unchanged $name\n";
-                    continue;
-                }
-                if ($overwriteAll) {
-                    echo "  overwrite $name\n";
-                } else {
-                    echo "      exist $name\n";
-                    echo "            ...overwrite? [Yes|No|All|Quit] ";
-                    $answer = trim(fgets(STDIN));
-                    if (!strncasecmp($answer, 'q', 1)) {
-                        return;
-                    } elseif (!strncasecmp($answer, 'y', 1)) {
-                        echo "  overwrite $name\n";
-                    } elseif (!strncasecmp($answer, 'a', 1)) {
-                        echo "  overwrite $name\n";
-                        $overwriteAll = true;
-                    } else {
-                        echo "       skip $name\n";
-                        continue;
-                    }
-                }
-            } else {
-                $this->ensureDirectory(dirname($target));
-                echo "   generate $name\n";
-            }
-            file_put_contents($target, $content);
-        }
-    }
-
-    /**
-     * Builds the file list of a directory.
-     * This method traverses through the specified directory and builds
-     * a list of files and subdirectories that the directory contains.
-     * The result of this function can be passed to {@link copyFiles}.
-     * @param string $sourceDir the source directory
-     * @param string $targetDir the target directory
-     * @param string $baseDir base directory
-     * @param array $ignoreFiles list of the names of files that should
-     * be ignored in list building process. Argument available since 1.1.11.
-     * @param array $renameMap hash array of file names that should be
-     * renamed. Example value: array('1.old.txt'=>'2.new.txt').
-     * Argument available since 1.1.11.
-     * @return array the file list (see {@link copyFiles})
-     */
-    public function buildFileList($sourceDir, $targetDir, $baseDir = '', $ignoreFiles = [], $renameMap = [])
-    {
-        $list = [];
-        $handle = opendir($sourceDir);
-        while (($file = readdir($handle)) !== false) {
-            if (in_array($file, ['.', '..', '.svn', '.gitignore']) || in_array($file, $ignoreFiles)) {
-                continue;
-            }
-            $sourcePath = $sourceDir . DIRECTORY_SEPARATOR . $file;
-            $targetPath = $targetDir . DIRECTORY_SEPARATOR . strtr($file, $renameMap);
-            $name = $baseDir === '' ? $file : $baseDir . '/' . $file;
-            $list[$name] = ['source' => $sourcePath, 'target' => $targetPath];
-            if (is_dir($sourcePath)) {
-                $list = array_merge($list, $this->buildFileList($sourcePath, $targetPath, $name, $ignoreFiles, $renameMap));
-            }
-        }
-        closedir($handle);
-        return $list;
-    }
-
-    /**
-     * Creates all parent directories if they do not exist.
-     * @param string $directory the directory to be checked
-     */
-    public function ensureDirectory($directory)
-    {
-        if (!is_dir($directory)) {
-            $this->ensureDirectory(dirname($directory));
-            echo "      mkdir " . strtr($directory, '\\', '/') . "\n";
-            mkdir($directory);
-        }
-    }
-
-    /**
-     * Renders a view file.
-     * @param string $_viewFile_ view file path
-     * @param array $_data_ optional data to be extracted as local view variables
-     * @return mixed the rendering result if required. Null otherwise.
-     */
-    public function renderFile($_viewFile_, $_data_ = null)
-    {
-        if (is_array($_data_)) {
-            extract($_data_, EXTR_PREFIX_SAME, 'data');
-        } else {
-            $data = $_data_;
-        }
-        ob_start();
-        ob_implicit_flush(false);
-        require($_viewFile_);
-        return ob_get_clean();
     }
 
     /**
